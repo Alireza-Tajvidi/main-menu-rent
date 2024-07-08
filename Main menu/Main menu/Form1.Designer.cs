f8846f89713bdf9844ebb2355dd21adc0e1d16fd
Alireza Tajvidi <A.tajvidi.official@gmail.com>
2024-07-08 17:07:34 +0300

    Core aspects of the main menu were designed.

---
+++ Main menu/Main menu/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace Main_menu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panTopbar = new System.Windows.Forms.Panel();
+            this.panOptions = new System.Windows.Forms.Panel();
+            this.panOpt1 = new System.Windows.Forms.Panel();
+            this.panOpt2 = new System.Windows.Forms.Panel();
+            this.panOpt3 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbTenantregistry = new System.Windows.Forms.Label();
+            this.panTenantregistry = new System.Windows.Forms.Panel();
+            this.lbAssetregistry = new System.Windows.Forms.Label();
+            this.panAssetregistry = new System.Windows.Forms.Panel();
+            this.lbNewcontract = new System.Windows.Forms.Label();
+            this.panNewcontract = new System.Windows.Forms.Panel();
+            this.lbCollection = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbLatency = new System.Windows.Forms.Label();
+            this.panLatency = new System.Windows.Forms.Panel();
+            this.lbLeaseend = new System.Windows.Forms.Label();
+            this.panLeaseend = new System.Windows.Forms.Panel();
+            this.lbLicense = new System.Windows.Forms.Label();
+            this.panLicense = new System.Windows.Forms.Panel();
+            this.picProfile = new System.Windows.Forms.PictureBox();
+            this.lbUsername_main = new System.Windows.Forms.Label();
+            this.lbWelcomemessage = new System.Windows.Forms.Label();
+            this.panTopbar.SuspendLayout();
+            this.panOptions.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picProfile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panTopbar
+            // 
+            this.panTopbar.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panTopbar.Controls.Add(this.picProfile);
+            this.panTopbar.Controls.Add(this.panOptions);
+            this.panTopbar.Location = new System.Drawing.Point(-5, -2);
+            this.panTopbar.Name = "panTopbar";
+            this.panTopbar.Size = new System.Drawing.Size(1016, 53);
+            this.panTopbar.TabIndex = 0;
+            // 
+            // panOptions
+            // 
+            this.panOptions.Controls.Add(this.panOpt3);
+            this.panOptions.Controls.Add(this.panOpt2);
+            this.panOptions.Controls.Add(this.panOpt1);
+            this.panOptions.Location = new System.Drawing.Point(17, 11);
+            this.panOptions.Name = "panOptions";
+            this.panOptions.Size = new System.Drawing.Size(39, 39);
+            this.panOptions.TabIndex = 1;
+            // 
+            // panOpt1
+            // 
+            this.panOpt1.BackColor = System.Drawing.Color.White;
+            this.panOpt1.Enabled = false;
+            this.panOpt1.Location = new System.Drawing.Point(3, 3);
+            this.panOpt1.Name = "panOpt1";
+            this.panOpt1.Size = new System.Drawing.Size(25, 5);
+            this.panOpt1.TabIndex = 1;
+            // 
+            // panOpt2
+            // 
+            this.panOpt2.BackColor = System.Drawing.Color.White;
+            this.panOpt2.Enabled = false;
+            this.panOpt2.Location = new System.Drawing.Point(3, 14);
+            this.panOpt2.Name = "panOpt2";
+            this.panOpt2.Size = new System.Drawing.Size(25, 5);
+            this.panOpt2.TabIndex = 1;
+            // 
+            // panOpt3
+            // 
+            this.panOpt3.BackColor = System.Drawing.Color.White;
+            this.panOpt3.Enabled = false;
+            this.panOpt3.Location = new System.Drawing.Point(3, 25);
+            this.panOpt3.Name = "panOpt3";
+            this.panOpt3.Size = new System.Drawing.Size(25, 5);
+            this.panOpt3.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.panel1.Controls.Add(this.panLicense);
+            this.panel1.Controls.Add(this.panLeaseend);
+            this.panel1.Controls.Add(this.panLatency);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.panNewcontract);
+            this.panel1.Controls.Add(this.panAssetregistry);
+            this.panel1.Controls.Add(this.panTenantregistry);
+            this.panel1.Controls.Add(this.lbLicense);
+            this.panel1.Controls.Add(this.lbLeaseend);
+            this.panel1.Controls.Add(this.lbLatency);
+            this.panel1.Controls.Add(this.lbCollection);
+            this.panel1.Controls.Add(this.lbNewcontract);
+            this.panel1.Controls.Add(this.lbAssetregistry);
+            this.panel1.Controls.Add(this.lbTenantregistry);
+            this.panel1.Location = new System.Drawing.Point(0, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 539);
+            this.panel1.TabIndex = 1;
+            // 
+            // lbTenantregistry
+            // 
+            this.lbTenantregistry.AutoSize = true;
+            this.lbTenantregistry.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTenantregistry.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(132)))), ((int)(((byte)(47)))));
+            this.lbTenantregistry.Location = new System.Drawing.Point(16, 19);
+            this.lbTenantregistry.Name = "lbTenantregistry";
+            this.lbTenantregistry.Size = new System.Drawing.Size(110, 24);
+            this.lbTenantregistry.TabIndex = 0;
+            this.lbTenantregistry.Text = "Kiracı kayıdı";
+            // 
+            // panTenantregistry
+            // 
+            this.panTenantregistry.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panTenantregistry.Location = new System.Drawing.Point(7, 46);
+            this.panTenantregistry.Name = "panTenantregistry";
+            this.panTenantregistry.Size = new System.Drawing.Size(190, 3);
+            this.panTenantregistry.TabIndex = 1;
+            // 
+            // lbAssetregistry
+            // 
+            this.lbAssetregistry.AutoSize = true;
+            this.lbAssetregistry.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAssetregistry.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(132)))), ((int)(((byte)(47)))));
+            this.lbAssetregistry.Location = new System.Drawing.Point(16, 68);
+            this.lbAssetregistry.Name = "lbAssetregistry";
+            this.lbAssetregistry.Size = new System.Drawing.Size(103, 24);
+            this.lbAssetregistry.TabIndex = 0;
+            this.lbAssetregistry.Text = "Mülk kayıdı";
+            // 
+            // panAssetregistry
+            // 
+            this.panAssetregistry.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panAssetregistry.Location = new System.Drawing.Point(7, 95);
+            this.panAssetregistry.Name = "panAssetregistry";
+            this.panAssetregistry.Size = new System.Drawing.Size(190, 3);
+            this.panAssetregistry.TabIndex = 1;
+            // 
+            // lbNewcontract
+            // 
+            this.lbNewcontract.AutoSize = true;
+            this.lbNewcontract.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNewcontract.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(132)))), ((int)(((byte)(47)))));
+            this.lbNewcontract.Location = new System.Drawing.Point(16, 119);
+            this.lbNewcontract.Name = "lbNewcontract";
+            this.lbNewcontract.Size = new System.Drawing.Size(108, 24);
+            this.lbNewcontract.TabIndex = 0;
+            this.lbNewcontract.Text = "Yeni kontrat";
+            // 
+            // panNewcontract
+            // 
+            this.panNewcontract.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panNewcontract.Location = new System.Drawing.Point(7, 146);
+            this.panNewcontract.Name = "panNewcontract";
+            this.panNewcontract.Size = new System.Drawing.Size(190, 3);
+            this.panNewcontract.TabIndex = 1;
+            // 
+            // lbCollection
+            // 
+            this.lbCollection.AutoSize = true;
+            this.lbCollection.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCollection.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(132)))), ((int)(((byte)(47)))));
+            this.lbCollection.Location = new System.Drawing.Point(16, 170);
+            this.lbCollection.Name = "lbCollection";
+            this.lbCollection.Size = new System.Drawing.Size(74, 24);
+            this.lbCollection.TabIndex = 0;
+            this.lbCollection.Text = "Tahsilat";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panel2.Location = new System.Drawing.Point(7, 197);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(190, 3);
+            this.panel2.TabIndex = 1;
+            // 
+            // lbLatency
+            // 
+            this.lbLatency.AutoSize = true;
+            this.lbLatency.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLatency.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(132)))), ((int)(((byte)(47)))));
+            this.lbLatency.Location = new System.Drawing.Point(16, 219);
+            this.lbLatency.Name = "lbLatency";
+            this.lbLatency.Size = new System.Drawing.Size(104, 24);
+            this.lbLatency.TabIndex = 0;
+            this.lbLatency.Text = "Gecikmeler";
+            // 
+            // panLatency
+            // 
+            this.panLatency.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panLatency.Location = new System.Drawing.Point(7, 246);
+            this.panLatency.Name = "panLatency";
+            this.panLatency.Size = new System.Drawing.Size(190, 3);
+            this.panLatency.TabIndex = 1;
+            // 
+            // lbLeaseend
+            // 
+            this.lbLeaseend.AutoSize = true;
+            this.lbLeaseend.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLeaseend.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(132)))), ((int)(((byte)(47)))));
+            this.lbLeaseend.Location = new System.Drawing.Point(16, 269);
+            this.lbLeaseend.Name = "lbLeaseend";
+            this.lbLeaseend.Size = new System.Drawing.Size(129, 24);
+            this.lbLeaseend.TabIndex = 0;
+            this.lbLeaseend.Text = "Bitecek kiralar";
+            // 
+            // panLeaseend
+            // 
+            this.panLeaseend.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panLeaseend.Location = new System.Drawing.Point(7, 296);
+            this.panLeaseend.Name = "panLeaseend";
+            this.panLeaseend.Size = new System.Drawing.Size(190, 3);
+            this.panLeaseend.TabIndex = 1;
+            // 
+            // lbLicense
+            // 
+            this.lbLicense.AutoSize = true;
+            this.lbLicense.Font = new System.Drawing.Font("Franklin Gothic Medium", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLicense.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(132)))), ((int)(((byte)(47)))));
+            this.lbLicense.Location = new System.Drawing.Point(16, 318);
+            this.lbLicense.Name = "lbLicense";
+            this.lbLicense.Size = new System.Drawing.Size(104, 24);
+            this.lbLicense.TabIndex = 0;
+            this.lbLicense.Text = "Lisanslama";
+            // 
+            // panLicense
+            // 
+            this.panLicense.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panLicense.Location = new System.Drawing.Point(7, 345);
+            this.panLicense.Name = "panLicense";
+            this.panLicense.Size = new System.Drawing.Size(190, 3);
+            this.panLicense.TabIndex = 1;
+            // 
+            // picProfile
+            // 
+            this.picProfile.Image = ((System.Drawing.Image)(resources.GetObject("picProfile.Image")));
+            this.picProfile.Location = new System.Drawing.Point(959, 7);
+            this.picProfile.Name = "picProfile";
+            this.picProfile.Size = new System.Drawing.Size(43, 39);
+            this.picProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picProfile.TabIndex = 2;
+            this.picProfile.TabStop = false;
+            // 
+            // lbUsername_main
+            // 
+            this.lbUsername_main.AutoSize = true;
+            this.lbUsername_main.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbUsername_main.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lbUsername_main.Location = new System.Drawing.Point(516, 278);
+            this.lbUsername_main.Name = "lbUsername_main";
+            this.lbUsername_main.Size = new System.Drawing.Size(123, 21);
+            this.lbUsername_main.TabIndex = 3;
+            this.lbUsername_main.Text = "\"Kullanıcı adı\"";
+            // 
+            // lbWelcomemessage
+            // 
+            this.lbWelcomemessage.AutoSize = true;
+            this.lbWelcomemessage.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbWelcomemessage.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.lbWelcomemessage.Location = new System.Drawing.Point(645, 278);
+            this.lbWelcomemessage.Name = "lbWelcomemessage";
+            this.lbWelcomemessage.Size = new System.Drawing.Size(86, 21);
+            this.lbWelcomemessage.TabIndex = 3;
+            this.lbWelcomemessage.Text = "Hoşgeldin";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1009, 588);
+            this.Controls.Add(this.lbWelcomemessage);
+            this.Controls.Add(this.lbUsername_main);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panTopbar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panTopbar.ResumeLayout(false);
+            this.panOptions.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picProfile)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panTopbar;
+        private System.Windows.Forms.Panel panOptions;
+        private System.Windows.Forms.Panel panOpt1;
+        private System.Windows.Forms.Panel panOpt3;
+        private System.Windows.Forms.Panel panOpt2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbTenantregistry;
+        private System.Windows.Forms.Panel panTenantregistry;
+        private System.Windows.Forms.Panel panAssetregistry;
+        private System.Windows.Forms.Label lbAssetregistry;
+        private System.Windows.Forms.Panel panNewcontract;
+        private System.Windows.Forms.Label lbNewcontract;
+        private System.Windows.Forms.Panel panLeaseend;
+        private System.Windows.Forms.Panel panLatency;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lbLeaseend;
+        private System.Windows.Forms.Label lbLatency;
+        private System.Windows.Forms.Label lbCollection;
+        private System.Windows.Forms.PictureBox picProfile;
+        private System.Windows.Forms.Panel panLicense;
+        private System.Windows.Forms.Label lbLicense;
+        private System.Windows.Forms.Label lbUsername_main;
+        private System.Windows.Forms.Label lbWelcomemessage;
+    }
+}
+
